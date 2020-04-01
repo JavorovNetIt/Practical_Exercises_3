14abdb534baaab194e0cc5a75934883e74b28077
JavorovNetIt <k.javorov.net.it@gmail.com>
2020-04-01 20:54:57 +0300

    Added new tasks

---
+++ Practical_Exercises_3/Multiply5Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Multiply5Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counterOfNegativeNumbers = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+
+                if (num1 < 0)
+                {
+                    counterOfNegativeNumbers++;
+                }
+                else if (num1 == 0)
+                {
+                    Console.WriteLine("The result is zero");
+                    return;
+                }
+            }
+
+            if (counterOfNegativeNumbers % 2 == 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else
+            {
+                Console.WriteLine("Negative");
+            }
+
+
+        }
+    }
+}
---
+++ Practical_Exercises_3/ThreeNums/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ThreeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int min = 0;
+            int aver = 0;
+            int max = 0;
+
+            if (num > num1 && num > num2)
+            {
+                max = num;
+
+                if (num1 > num2)
+                {
+                    aver = num1;
+                    min = num2;
+                }
+                else
+                {
+                    aver = num2;
+                    min = num1;
+                }
+            }
+            else if (num1 > num && num1 > num2)
+            {
+                max = num1;
+
+                if (num > num2)
+                {
+                    aver = num;
+                    min = num2;
+                }
+                else
+                {
+                    aver = num2;
+                    min = num;
+                }
+            }
+            else if (num2 > num && num2 > num1)
+            {
+                max = num2;
+
+                if (num > num1)
+                {
+                    aver = num;
+                    min = num1;
+                }
+                else
+                {
+                    aver = num1;
+                    min = num;
+                }
+            }
+
+            Console.WriteLine($"{max} {aver} {min}");
+        }
+    }
+}
